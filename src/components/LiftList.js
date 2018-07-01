b2712903be136e9b82655bdd762e939b7ecbc2df
jantimpe <timpe31@gmail.com>
2018-07-01 12:50:53 -0500

    refactored styling

--- src/components/LiftList.js
+++ src/components/LiftList.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
-import '../style/LiftList.css'
-
 import Lift from './Lift'
+
 
 export default class LiftList extends Component {
 
--- src/components/LiftResult.js
+++ src/components/LiftResult.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import '../style/LiftResult.css'
 
 
 export default class LiftResult extends Component {
