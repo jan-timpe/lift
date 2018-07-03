9d507a330e5bd93b1f833de66f107d878f0f239a
jantimpe <timpe31@gmail.com>
2018-07-03 11:29:25 -0500

    test: added liftresultlist tests

--- src/__tests__/components/LiftResultList.js
+++ src/__tests__/components/LiftResultList.js
@@ -1,7 +1,26 @@
-
+import React from 'react'
+import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json'
+import LiftResultList from '../../components/LiftResultList'
 
 describe(`LiftResultList`, () => {
-  it(`should pass`, () => {
-    
+
+  // snapshot
+  it(`should render`, () => {
+    const results = [{
+      id: 0,
+      liftId: 0,
+      weight: 200,
+      date: '7/1/2018'
+    }, {
+      id: 1,
+      liftId: 0,
+      weight: 300,
+      date: '7/2/2018'
+    }]
+    const wrapper = shallow(<LiftResultList results={results} />)
+    const component = wrapper
+    expect(toJson(component)).toMatchSnapshot()
   })
+
 })
