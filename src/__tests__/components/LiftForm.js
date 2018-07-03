937b26c4951cab35d9be71d953b36f0b757fb2d7
jantimpe <timpe31@gmail.com>
2018-07-03 07:32:58 -0500

    test: added liftform tests

--- src/__tests__/components/LiftForm.js
+++ src/__tests__/components/LiftForm.js
@@ -1,7 +1,29 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json'
+import LiftForm from '../../components/LiftForm'
 
 
 describe(`LiftForm`, () => {
-  it(`should pass`, () => {
-    
+
+  // snapshot
+  it(`should render`, () => {
+    const mockAddLift = jest.fn()
+    const wrapper = shallow(<LiftForm addLift={mockAddLift}/>)
+    const component = wrapper
+    expect(toJson(component)).toMatchSnapshot()
   })
+
+
+  // form
+  it(`should call addLift on submit`, () => {
+    const mockAddLift = jest.fn()
+    const preventDefault = jest.fn()
+    const wrapper = shallow(<LiftForm addLift={mockAddLift}/>)
+    const component = wrapper
+
+    component.find('form').simulate('submit', { preventDefault })
+    expect(mockAddLift.mock.calls.length).toEqual(1)
+  })
+
 })
