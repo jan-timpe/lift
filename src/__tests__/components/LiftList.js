4892767409b30b47a6c65cf68761bb29b71cc6c0
jantimpe <timpe31@gmail.com>
2018-07-03 11:19:30 -0500

    test: added liftlist test

--- src/__tests__/components/LiftList.js
+++ src/__tests__/components/LiftList.js
@@ -1,7 +1,23 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json'
+import LiftList from '../../components/LiftList'
 
 
 describe(`LiftList`, () => {
-  it(`should pass`, () => {
-    
+
+  // snapshot
+  it(`should render`, () => {
+    const lifts = [{
+      id: 0,
+      name: 'Squat'
+    }, {
+      id: 1,
+      name: 'Bench'
+    }]
+    const wrapper = shallow(<LiftList lifts={lifts} />)
+    const component = wrapper
+    expect(toJson(component)).toMatchSnapshot()
   })
+
 })
--- src/components/LiftList.js
+++ src/components/LiftList.js
@@ -13,4 +13,5 @@ export default class LiftList extends Component {
       </div>
     )
   }
+
 }
