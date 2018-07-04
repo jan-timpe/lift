b82d06394a22f7a8ec9c82a187a4d4beda837b1f
jantimpe <timpe31@gmail.com>
2018-07-04 13:35:06 -0500

    test: added lift reducer test

--- src/__tests__/reducers/lift.js
+++ src/__tests__/reducers/lift.js
@@ -1,7 +1,27 @@
-
+import reducer from '../../reducers/lift'
+import { ADD_LIFT } from '../../actions'
 
 describe(`LiftListContainer`, () => {
-  it(`should pass`, () => {
-    
+
+  // initial state
+  it(`should return empty initial state`, () => {
+    expect(reducer(undefined, {})).toEqual([])
   })
+
+
+  // should handle adding lift
+  it(`should handle ADD_LIFT`, () => {
+
+    const action = {
+      type: ADD_LIFT,
+      id: 0,
+      name: 'Squat'
+    }
+
+    expect(reducer([], action)).toEqual([{
+      id: 0,
+      name: 'Squat'
+    }])
+  })
+
 })
