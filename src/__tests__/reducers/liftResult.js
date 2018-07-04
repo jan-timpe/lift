e166e179a216141cd4c38485720efa1da90d332c
jantimpe <timpe31@gmail.com>
2018-07-04 13:37:27 -0500

    test: added lift result reducer tests

--- src/__tests__/reducers/liftResult.js
+++ src/__tests__/reducers/liftResult.js
@@ -1,7 +1,31 @@
+import reducer from '../../reducers/liftResult'
+import { ADD_LIFT_RESULT } from '../../actions'
 
 
 describe(`LiftListContainer`, () => {
-  it(`should pass`, () => {
-    
+
+  // initial state
+  it(`should return empty initial state`, () => {
+    expect(reducer(undefined, {})).toEqual([])
   })
+
+
+  // add lift result
+  it(`should handle ADD_LIFT_RESULT`, () => {
+    const action = {
+      type: ADD_LIFT_RESULT,
+      id: 0,
+      liftId: 0,
+      weight: 200,
+      date: '7/1/2018'
+    }
+
+    expect(reducer([], action)).toEqual([{
+      id: 0,
+      liftId: 0,
+      weight: 200,
+      date: '7/1/2018'
+    }])
+  })
+
 })
